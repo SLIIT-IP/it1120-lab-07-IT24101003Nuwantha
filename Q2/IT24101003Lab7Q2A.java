6120fdab8c62c4504d1a1dc2910ae07bbb98ef84
IT24101003Nuwantha <it24101003@my.sliit.lk>
2024-09-18 09:46:05 +0530

    Add IT24101003Lab7Q2A.java file to Q2

---
+++ Q2/IT24101003Lab7Q2A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IT24101003Lab7Q2A{
+	
+	public static void main(String []args){
+		
+		for(int i=1; i<=4;i++){
+			for(int y=1; y<=5;y++){
+				System.out.print("$");
+			}
+			System.out.println("");
+		}
+		}		
+		
+	}
