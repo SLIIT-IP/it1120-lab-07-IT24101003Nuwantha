70005cd425fb1bb17c1b12c2a5903550c3215ef7
IT24101003Nuwantha <it24101003@my.sliit.lk>
2024-09-18 09:10:49 +0530

    Add IT24101003Lab7Q1.java file to Q1

---
+++ Q1/IT24101003Lab7Q1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class IT24101003Lab7Q1{
+	
+	public static void main(String []args){
+		int count = 1;
+		int total=0;
+		Scanner scanner = new Scanner(System.in);
+		while(count <=4){
+			System.out.print("Enter subject Mark " + count +": ");
+			int mark = scanner.nextInt();
+			total += mark;
+			count++;
+		}
+		  
+		   float avg = total /4;
+	
+		   System.out.println("Average is " + avg);
+		   if(avg<=100 && avg >= 75){
+			   System.out.println("overall grade is Distinction");
+		   }else if(avg<75 && avg >= 50){
+			   System.out.println("overall grade is Credit");
+		   }else{
+			   System.out.println("overall grade is Fail");
+		   }
+		
+		
+		
+		
+		
+	}
+} 
